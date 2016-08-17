645be02a7b5a5d3a02502ae8da0e92c4e1fb0e86
Calvin Kwong <ckwong93@gmail.com>
2016-08-17 09:33:46 -0700

    Add in location functionality with updated color scheme

--- ArchitectWorld/js/modelatgeolocation.js
+++ ArchitectWorld/js/modelatgeolocation.js
@@ -1,39 +1,46 @@
 var World = {
-
+    
 	PATH_INDICATOR : "assets/directionIndicator.png",
 	PATH_MODEL_WT3 : "assets/arrow.wt3",
 	MSG_MODEL_VISIBLE : "Thread Ahead!",
 	MSG_MODEL_NOTVISIBLE : "Invisible Thread!",
-
+    
+   
+    
 	init: function initFn() {
-		
-		// wait for first location signal before creating the experience to ensure you know user's location during setup process
-		AR.context.onLocationChanged = function(latitude, longitude, altitude, accuracy){
-
-			// store user's location so you have access to it at any time
-			World.myLocation = {"latitude": latitude, "longitude" : longitude, "altitude" : altitude };
-
-			// create model around the user on very first location update
-			if (!World.created) {
-				document.getElementById('loadingMessage').innerHTML = 'Loading...';
-				World.created = true;
-				World.createModelAtLocation(World.myLocation);
-			}
-		};
-
+        // Call server for last location
+        $.ajax({
+               url: "https://evening-plateau-54300.herokuapp.com/sushi.json",
+               dataType: "json",
+               type: "GET"
+               }).done(function(response){
+                    AR.context.onLocationChanged = function(latitude, longitude, altitude, accuracy){
+                      World.myLocation = {"latitude": latitude, "longitude" : longitude, "altitude" : altitude };
+                      if (!World.created) {
+                         var serverLatitude = parseFloat(response["thread"][0]);
+                         var serverLongitude = parseFloat(response["thread"][1]);
+                         document.getElementById('loadingMessage').innerHTML = "Latitude:"+serverLatitude+ "Longitude:"+serverLongitude;
+                         World.created = true;
+                         World.createModelAtLocation(World.myLocation, serverLatitude, serverLongitude);
+                      }
+                    };
+                })
 		
 	},
 
-	createModelAtLocation: function createModelAtLocationFn(location) {
-
-        var myHouse = new AR.GeoLocation(37.784277, -122.403224)
+	createModelAtLocation: function createModelAtLocationFn(location, lat, long) {
+        
+        var myHouse = new AR.GeoLocation(lat, long)
         var distance = myHouse.distanceToUser();
+        var distance_model = distance > 500 ? [World.model] : [World.indicatorDrawable2];
 
 		// place object around the user, in this case just a few meters next to the user. Note that the object will stay where it is, so user can move around it.
 		World.modelGeoLocation = new AR.GeoLocation(myHouse.latitude, myHouse.longitude, AR.CONST.UNKNOWN_ALTITUDE);
 		
-		// load model
         
+        
+        
+		// load model
 		World.model = new AR.Model(World.PATH_MODEL_WT3, {
 
 			// trigger animation once asset was loaded
@@ -61,6 +68,7 @@ var World = {
                                                         
 		               indicator: [World.indicatorDrawable],
                        cam : [World.indicatorDrawable2]
+                       
 		            },
 		            onEnterFieldOfVision: function() {
 		            	World.modelVisible = true;
@@ -93,7 +101,7 @@ var World = {
 				y: 0,
 				z: 0
 			},
-			rotate: {
+			rotate: { 
                 roll:-25, //z
 				heading: 90, //x
 				tilt: 180 //y
@@ -108,13 +116,40 @@ var World = {
 
 	worldError: function worldErrorFn(msg) {
 		document.getElementById('loadingMessage').innerHTML = msg ? msg : 'unexpected error';
-	}
+	},
+    
+    // get request to the server to show the last location sent to the server
+    showLocation: function showLocationFn() {
+        
+            $.ajax({
+                   url: "https://evening-plateau-54300.herokuapp.com/sushi.json",
+                   dataType: "json",
+                   type: "GET"
+                   }).done(function(response){
+                        document.getElementById('loc').innerHTML = "Server response: " + response["thread"][0] + response["thread"][1];
+                    })
+        
+    },
+
+    // post request to the server to save the current location
+    saveLocation: function saveLocationFn() {
+        
+        var latitude = World.myLocation.latitude;
+        var longitude = World.myLocation.longitude;
+        $.ajax({
+               url: "https://evening-plateau-54300.herokuapp.com/latitude/" + latitude + "/longitude/" +longitude,
+               type: "POST"
+        }).done(function(response){
+                
+                })
+    }
+    
 };
 
 World.init();
 
-var current_location = new AR.Geolocation(World.myLocation.latitude,World.myLocation.longitude);
-var model_location = new AR.Geolocation(37.780694, -122.403704);
-var distance = model_location.distanceTo(current_location);
-console.log(distance);
+//var current_location = new AR.Geolocation(World.myLocation.latitude,World.myLocation.longitude);
+//var model_location = new AR.Geolocation(37.780694, -122.403704);
+//var distance = model_location.distanceTo(current_location);
+//console.log(distance);
 
